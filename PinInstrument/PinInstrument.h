676a26bd341db2f2086778a05929f067c39566d9
warsier <787012293@qq.com>
2019-03-14 12:16:06 -0400

    temp

--- PinInstrument/PinInstrument.h
+++ PinInstrument/PinInstrument.h
@@ -6,32 +6,54 @@
 //
 //===----------------------------------------------------------------------===//
 
+
 #ifndef __PININSTRUMENT_H__
 #define __PININSTRUMENT_H__
 
 #include "pin.H"
 
 namespace PIMProf {
+    
+    const UINT32 MAX_INDEX = 4096;
+    const UINT32 INDEX_SPECIAL =  3000;
+    const UINT32 MAX_MEM_SIZE = 512;
+
+    const UINT32 INDEX_TOTAL =          INDEX_SPECIAL + 0;
+    const UINT32 INDEX_MEM_ATOMIC =     INDEX_SPECIAL + 1;
+    const UINT32 INDEX_STACK_READ =     INDEX_SPECIAL + 2;
+    const UINT32 INDEX_STACK_WRITE =    INDEX_SPECIAL + 3;
+    const UINT32 INDEX_IPREL_READ =     INDEX_SPECIAL + 4;
+    const UINT32 INDEX_IPREL_WRITE =    INDEX_SPECIAL + 5;
+    const UINT32 INDEX_MEM_READ_SIZE =  INDEX_SPECIAL + 6;
+    const UINT32 INDEX_MEM_WRITE_SIZE = INDEX_SPECIAL + 6 + MAX_MEM_SIZE;
+    const UINT32 INDEX_SPECIAL_END   =  INDEX_SPECIAL + 6 + MAX_MEM_SIZE + MAX_MEM_SIZE;
+
+
     class InstructionLatency {
-    public:
-        vector<UINT32> latencytable;
+    private:
+        /// Construction of latency table follows the opcode generation function in
+        /// $(PIN_ROOT)/source/tools/SimpleExamples/opcodemix.cpp.
+        UINT32 latencytable[MAX_INDEX];
 
     public:
-        void InstructionLatency();
+        /// Default initialization.
+        /// Initialize latencytable with hard-coded instruction latency.
+        InstructionLatency();
 
-        void InstructionLatency(std::ofstream &);
+        /// Initialization with input config.
+        InstructionLatency(std::ofstream &);
     
     public:
         void ReadConfigFromFile();
         void WriteConfigToFile();
 
-        /// Generate the default instruction latency config.
+        /// Print the default instruction latency config.
         /// This is the default instruction latency config PIMProf will use.
-        void GenerateConfigTemplate();
+        void PrintConfigTemplate();
 
         /// Generate a template of instruction latency config for the given program.
         /// This function will pre-execute the given program once.
-        /// filename: the fstream of input program file.
+        /// filename: the filename of input program file.
         void GenerateConfigTemplate(const std::string &filename);
 
     };

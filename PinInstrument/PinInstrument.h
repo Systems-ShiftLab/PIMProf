e0903eaf6ebe636980022c930601343b9fb224d1
warsier <787012293@qq.com>
2019-03-17 12:11:33 -0400

    temp

--- PinInstrument/PinInstrument.h
+++ PinInstrument/PinInstrument.h
@@ -15,18 +15,18 @@
 namespace PIMProf {
     
     const UINT32 MAX_INDEX = 4096;
-    const UINT32 INDEX_SPECIAL =  3000;
+    // const UINT32 INDEX_SPECIAL = 3000;
     const UINT32 MAX_MEM_SIZE = 512;
 
-    const UINT32 INDEX_TOTAL =          INDEX_SPECIAL + 0;
-    const UINT32 INDEX_MEM_ATOMIC =     INDEX_SPECIAL + 1;
-    const UINT32 INDEX_STACK_READ =     INDEX_SPECIAL + 2;
-    const UINT32 INDEX_STACK_WRITE =    INDEX_SPECIAL + 3;
-    const UINT32 INDEX_IPREL_READ =     INDEX_SPECIAL + 4;
-    const UINT32 INDEX_IPREL_WRITE =    INDEX_SPECIAL + 5;
-    const UINT32 INDEX_MEM_READ_SIZE =  INDEX_SPECIAL + 6;
-    const UINT32 INDEX_MEM_WRITE_SIZE = INDEX_SPECIAL + 6 + MAX_MEM_SIZE;
-    const UINT32 INDEX_SPECIAL_END   =  INDEX_SPECIAL + 6 + MAX_MEM_SIZE + MAX_MEM_SIZE;
+    // const UINT32 INDEX_TOTAL =          INDEX_SPECIAL + 0;
+    // const UINT32 INDEX_MEM_ATOMIC =     INDEX_SPECIAL + 1;
+    // const UINT32 INDEX_STACK_READ =     INDEX_SPECIAL + 2;
+    // const UINT32 INDEX_STACK_WRITE =    INDEX_SPECIAL + 3;
+    // const UINT32 INDEX_IPREL_READ =     INDEX_SPECIAL + 4;
+    // const UINT32 INDEX_IPREL_WRITE =    INDEX_SPECIAL + 5;
+    // const UINT32 INDEX_MEM_READ_SIZE =  INDEX_SPECIAL + 6;
+    // const UINT32 INDEX_MEM_WRITE_SIZE = INDEX_SPECIAL + 6 + MAX_MEM_SIZE;
+    // const UINT32 INDEX_SPECIAL_END   =  INDEX_SPECIAL + 6 + MAX_MEM_SIZE + MAX_MEM_SIZE;
 
 
     class InstructionLatency {
@@ -41,20 +41,20 @@ namespace PIMProf {
         InstructionLatency();
 
         /// Initialization with input config.
-        InstructionLatency(std::ofstream &);
+        InstructionLatency(std::istream &in);
     
     public:
         void ReadConfigFromFile();
         void WriteConfigToFile();
 
-        /// Print the default instruction latency config.
+        /// Print the default instruction latency config to ofstream.
         /// This is the default instruction latency config PIMProf will use.
-        void PrintConfigTemplate();
+        void PrintConfigTemplate(ostream& out);
 
         /// Generate a template of instruction latency config for the given program.
         /// This function will pre-execute the given program once.
         /// filename: the filename of input program file.
-        void GenerateConfigTemplate(const std::string &filename);
+        void GenerateConfigTemplateFromFile(const std::string &filename);
 
     };
 

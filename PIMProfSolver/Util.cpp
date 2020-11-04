4a953b10dcaaeb24bea1c350e05640e9a70464e6
warsier <787012293@qq.com>
2020-11-03 20:35:34 -0500

    change name

---
+++ PIMProfSolver/Util.cpp
@@ -0,0 +1,101 @@
+//===- Util.cpp - Utils for instrumentation ------------*- C++ -*-===//
+//
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#include "Util.h"
+#include <getopt.h>
+
+using namespace PIMProf;
+
+void Usage()
+{
+    infomsg("Usage: ./Solver.exe <mode> <mode_specific_params>");
+    infomsg("Select mode from: mpki, para, reuse");
+    infomsg("Mode specific parameters:");
+    infomsg("mpki mode: ./Solver.exe mpki <cpu_stats_file> <pim_stats_file> <output_file>");
+    infomsg("para mode: ./Solver.exe para <cpu_stats_file> <pim_stats_file> <output_file>");
+    infomsg("reuse mode: ./Solver.exe reuse <cpu_stats_file> <pim_stats_file> <reuse_file> <output_file>");
+    exit(0);
+}
+
+void CommandLineParser::initialize(int argc, char *argv[])
+{
+    auto parser = [&](const char* const short_opt, const option long_opt[]) {
+        while (true) {
+            const auto opt = getopt_long(argc, argv, short_opt, long_opt, nullptr);
+            if (opt == -1) break;
+
+            switch (opt)
+            {
+            case 'c':
+                _cpustatsfile = std::string(optarg); std::cout << "c " << _cpustatsfile << std::endl; break;
+            case 'p':
+                _pimstatsfile = std::string(optarg); std::cout << "p " << _pimstatsfile << std::endl; break;
+            case 'r':
+                _reusefile = std::string(optarg); std::cout << "r " << _reusefile << std::endl; break;
+            case 'o':
+                _outputfile = std::string(optarg); std::cout << "o " << _outputfile << std::endl; break;
+            case 'h': // -h or --help
+            case '?': // Unrecognized option
+            default:
+                Usage();
+            }
+        }
+    };
+
+    
+    if (argc <= 1) { Usage(); }
+    std::string _mode_string(argv[1]);
+    optind++;
+    if (_mode_string == "mpki") {
+        _mode = Mode::MPKI;
+        const char* const short_opt = "c:p:o:h";
+        const option long_opt[] = {
+            {"cpu", required_argument, nullptr, 'c'},
+            {"pim", required_argument, nullptr, 'p'},
+            {"output", required_argument, nullptr, 'o'},
+            {"help", no_argument, nullptr, 'h'},
+            {nullptr, no_argument, nullptr, 0}
+        };
+        parser(short_opt, long_opt);
+        if (_cpustatsfile == "" || _pimstatsfile == "" || _outputfile == "") {
+            Usage();
+        }
+    }
+    else if (_mode_string == "para") {
+        _mode = Mode::PARA;
+        assert(0);
+    }
+    else if (_mode_string == "reuse") {
+        _mode = Mode::REUSE;
+        const char* const short_opt = "c:p:r:o:h";
+        const option long_opt[] = {
+            {"cpu", required_argument, nullptr, 'c'},
+            {"pim", required_argument, nullptr, 'p'},
+            {"reuse", required_argument, nullptr, 'r'},
+            {"output", required_argument, nullptr, 'o'},
+            {"help", no_argument, nullptr, 'h'},
+            {nullptr, no_argument, nullptr, 0}
+        };
+        parser(short_opt, long_opt);
+        if (_cpustatsfile == "" || _pimstatsfile == "" || _reusefile == "" || _outputfile == "") {
+            Usage();
+        }
+    }
+    else {
+        Usage();
+    }
+}
+
+void PIMProf::PrintInstruction(std::ostream *out, uint64_t insAddr, std::string insDis, uint32_t simd_len) {
+    *out << std::hex << insAddr << std::dec << ", " << insDis << " " << simd_len << std::endl;
+    // *out << insDis << std::endl;
+}
+
+void PIMProf::PrintInfo(std::ostream *out, std::string info) {
+    *out << info << std::endl;
+}
---
+++ PIMProfSolver/Util.h
@@ -0,0 +1,151 @@
+//===- Util.h - Utils for instrumentation ------------*- C++ -*-===//
+//
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//===----------------------------------------------------------------------===//
+#ifndef __PINUTIL_H__
+#define __PINUTIL_H__
+
+#include <stack>
+#include <algorithm>
+#include <iostream>
+#include <bitset>
+#include <cassert>
+#include <cstdarg>
+
+#include "../LLVMAnalysis/Common.h"
+#include "INIReader.h"
+
+namespace PIMProf {
+
+const BBLID GLOBAL_BBLID = -1;
+
+enum ACCESS_TYPE
+{
+    ACCESS_TYPE_LOAD,
+    ACCESS_TYPE_STORE,
+    MAX_ACCESS_TYPE
+};
+
+/* ===================================================================== */
+/* BBLScope */
+/* ===================================================================== */
+class BBLScope {
+  private:
+    std::stack<BBLID> bblidstack;
+  public:
+    inline BBLScope()
+    {
+        // we set the global BBL ID as 0
+        bblidstack.push(GLOBAL_BBLID);
+    }
+    inline void push(BBLID bblid)
+    {
+        bblidstack.push(bblid);
+    }
+    inline void pop()
+    {
+        bblidstack.pop();
+    }
+    inline BBLID top()
+    {
+        return bblidstack.top();
+    }
+};
+
+/* ===================================================================== */
+/* Output Formatting */
+/* ===================================================================== */
+
+const std::string REDCOLOR = "\033[0;31m";
+const std::string GREENCOLOR = "\033[0;32m";
+const std::string YELLOWCOLOR = "\033[0;33m";
+const std::string NOCOLOR = "\033[0m";
+
+#define PRETTY_PRINT_FUNC_HELPER(TYPE, COLOR) \
+inline int TYPE##msg(const char *format, ...) \
+{ \
+    va_list args; \
+    va_start(args, format); \
+    std::string output = COLOR + "## PIMProf " + #TYPE + ": " + NOCOLOR + std::string(format) + "\n"; \
+    int result = vprintf(output.c_str(), args); \
+    va_end(args); \
+    return result; \
+}
+
+PRETTY_PRINT_FUNC_HELPER(info, GREENCOLOR)
+PRETTY_PRINT_FUNC_HELPER(error, REDCOLOR)
+PRETTY_PRINT_FUNC_HELPER(warning, YELLOWCOLOR)
+
+/* ===================================================================== */
+/* CommandLineParser */
+/* ===================================================================== */
+
+class CommandLineParser {
+  public:
+    enum Mode {
+        MPKI, PARA, REUSE
+    };
+  private:
+    std::string _cpustatsfile, _pimstatsfile;
+    std::string _reusefile;
+    std::string _outputfile;
+    Mode _mode;
+
+  public:
+    void initialize(int argc, char *argv[]);
+
+    inline std::string cpustatsfile() { return _cpustatsfile; }
+    inline std::string pimstatsfile() { return _pimstatsfile; }
+    inline std::string reusefile() { return _reusefile; }
+    inline std::string outputfile() { return _outputfile; }
+    inline Mode mode() { return _mode; }
+    inline bool enableglobalbbl() { return true; } // whether considering the dependency with the global BBL, for debug use
+
+};
+
+/* ===================================================================== */
+/* ConfigReader */
+/* ===================================================================== */
+
+class ConfigReader: public INIReader {
+  private:
+    std::string filename;
+  public:
+    inline ConfigReader() {}
+
+    inline ConfigReader(std::string f)
+        : INIReader(f), filename(f)
+    {
+        // int32_t error = ParseError();
+        // if (error == -1) {
+        //     errormsg() << ".ini file: Open error." << std::endl;
+        // }
+        // else if (error == -2) {
+        //     errormsg() << ".ini file: Memory allocation error." << std::endl;
+        // } 
+        
+        // else if (error > 0) {
+        //     errormsg() << ".ini file: Parsing failure on line "
+        //         << error 
+        //         << "." << std::endl;
+        // }
+        // if (error) {
+        //     errormsg() << "Filename: " << filename << std::endl;
+        //     assert(0);
+        // }
+    }
+};
+
+/* ===================================================================== */
+/* InstructionPrinter */
+/* ===================================================================== */
+
+void PrintInstruction(std::ostream *out, uint64_t insAddr, std::string insDis, uint32_t simd_len);
+void PrintInfo(std::ostream *out, std::string info);
+
+}// namespace PIMProf
+
+#endif // __PINUTIL_H__
